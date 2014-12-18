4b9eb2dc6783a93ae9ecdf3a67b605e6656be699
Juriy Bura <juriy.bura@gmail.com>
2014-12-18 17:12:45 +0800

    minimal app

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var errorHandler = require('errorHandler');
 
 var app = express();
 
-app.use(function(req, res, next) {
-	res.send('Hello World');
+var sessionMiddleware = session({
+	secret: 'keyboardcat',
+	saveUninitialized: true,
+	resave: true
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use(sessionMiddleware);
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/login', function(req, res, next) {
+	// check names here
+	req.session.login = req.body.login
+	res.redirect('/chat.html');
+});
+
+app.get('/session', function(req, res) {
+	res.json(req.session);
 });
 
 app.listen(3000);
