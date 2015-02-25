fe4182f6f45570ea8f05150949be0e0f406ce103
Juriy Bura <juriy.bura@gmail.com>
2015-02-25 19:43:44 +0800

    Added more statements

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 var cup = {
-	material: "glass",
+	material: "metal",
 	isHot: false,
 	color: 0xCCC
 };
@@ -10,7 +10,14 @@ cup.contents = 'coffee';
 function printCup(cupToPrint) {
 	console.log("This is a cup of",
 		cupToPrint.contents);
-	console.log("This cup is", getHotAdjective(cupToPrint));
+	console.log("This cup is", 
+		getHotAdjective(cupToPrint));
+	console.log("This cup is made of", 
+		cupToPrint.material);
+	
+	if (cupToPrint.material == "glass") {
+		console.log("This cup is fragile");
+	}
 }
 
 function getHotAdjective(cupToPrint) {
