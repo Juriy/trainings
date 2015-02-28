2dfe3914dec583b6d3f3f1bbb2fa6955daab5f6f
Juriy Bura <juriy.bura@gmail.com>
2015-03-01 00:22:27 +0800

    Added day 4

--- main.js
+++ main.js
@@ -1,51 +1,7 @@
-var favBook = {
-	title: "The Hobbit",
-	numPages: 123,
-	isInteresting: true,
-	author: {
-		name: "J.R.R. Tolkien",
-		dateOfBirth: ....
-		country: "England"
-	}
-};
-
-var state = 1;
-
-function openUserInput() {
-	process.stdin.setEncoding('utf-8');
-	process.stdin.resume();
-	process.stdin.on('data', onUserInput);	
+function avg(a, b) {
+	return (a + b)/2;
 }
+// console.log(avg("10", "20")); // prints 15
+// console.log(avg("0", "1")); // prints 0.5
 
-function printBanner() {
-	console.log("Hello, welcome to Book Worm!");
-	console.log("type exit to exit");
-	console.log("Enter a title of your favorite book");
-}
-
-function onUserInput(input) {
-	input = normalizeInput(input)
-	if (input == "exit") {
-		process.exit();
-	} else {
-		
-		if (state == 1) { // title 
-			favBook.title = input;
-			state = 2;
-			console.log("Please enter the number of pages");
-
-		} else if (state == 2) { // number of pages
-			favBook.numPages = +input;
-		}
-		
-		// 
-		console.log(favBook.title, favBook.numPages);
-	}
-}
-
-function normalizeInput(input) {
-	return input.trim();
-}
-
-openUserInput();
-printBanner();
+console.log(+'031')
