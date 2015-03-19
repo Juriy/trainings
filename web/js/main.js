422928663162209cdd37cc1d2f54dbcda650438a
Juriy Bura <juriy.bura@gmail.com>
2015-03-19 20:15:20 +0800

    Added templating for book catalog

--- web/js/main.js
+++ web/js/main.js
@@ -1,2 +1,25 @@
-var el = document.getElementById('bookHeader');
-el.innerHTML = 'total books ' + books.length;
+var parent = document.querySelector('.book-catalog');
+
+books.forEach(addBook);
+
+function addBook(book) {
+	var el = document.createElement('li');
+	var template = getBookTemplate();
+	template = template
+		.replace('{{BOOK_NAME}}', book.title)
+		.replace('{{IMAGE}}', book.images.medium)
+		.replace('{{DESCRIPTION}}', book.description);
+
+	el.innerHTML = template;
+	parent.appendChild(el);
+}
+
+function getBookTemplate() {
+	var templateElement = 
+		document.getElementById('book-template');
+	return templateElement.innerHTML;
+}
+
+
+// el.innerHTML = books[0].title;
+
