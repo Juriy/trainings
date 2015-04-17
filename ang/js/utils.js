7499ff984eb709342dd8959a809feff13a7b4a81
Juriy Bura <juriy.bura@gmail.com>
2015-04-17 21:08:25 +0800

    Added angular catalog

--- ang/js/utils.js
+++
@@ -1,9 +0,0 @@
-function trim(text, maxLen) {
-	var cut = text.substr(0, maxLen);
-	return cut.substr(0, 
-		Math.min(cut.length, cut.lastIndexOf(' ')));
-}
-
-function asPlainText(html) {
-	return html ? html.replace(/<(?:.|\n)*?>/gm, '') : '';
-}
